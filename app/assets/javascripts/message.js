f4cd9300491c9bfa9a441ef606eeeeef7b5690df
akino102819 <k.akino1998@icloud.com>
2020-02-28 20:34:19 +0900

    メッセージ機能の非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -52,8 +52,10 @@ $(function(){
       $('.messages').append(html);      
       $('form')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
-      $('.form__input__send').prop( 'disabled', false )
     })
+    .always(function(){
+      $('.form__input__send').prop( 'disabled', false )
+    }
     .fail(function() {
       alert("メッセージ送信に失敗しました");
   });
